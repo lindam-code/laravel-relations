e2e2b2989026cdc5a62ba5bb89cf844b551c2801
lindam <linda.minotti2@gmail.com>
2020-09-10 18:52:07 +0200

    Grafica pagina edit.

--- resources/views/albums/edit.blade.php
+++ resources/views/albums/edit.blade.php
@@ -1,34 +1,40 @@
-<h2>Edit album</h2>
+@extends('layouts.layout')
+@section('content')
+  <div class="main_wrapper">
+    <h2>Edit album</h2>
+    <div class="form_wrapper">
+      {{-- Form per modificare i dati degli album--}}
+      <form action="{{ route('albums.update',$album->id) }}" method="post">
+        @csrf
+        @method('PUT')
+        <div >
+          <label>Title</label>
+          <input type="text" name="title" value="{{ $album->title }}">
+        </div>
 
-{{-- Form per modificare i dati degli album--}}
-<form action="{{ route('albums.update',$album->id) }}" method="post">
-  @csrf
-  @method('PUT')
-  <div >
-    <label>Title</label>
-    <input type="text" name="title" value="{{ $album->title }}">
-  </div>
+        <div >
+          <label>Artist</label>
+          <input type="text" name="artist" value="{{ $album->artist }}">
+        </div>
 
-  <div >
-    <label>Artist</label>
-    <input type="text" name="artist" value="{{ $album->artist }}">
-  </div>
+        <div >
+          <label>Year</label>
+          <input type="number" name="year" value="{{ $album->year }}">
+        </div>
 
-  <div >
-    <label>Year</label>
-    <input type="number" name="year" value="{{ $album->year }}">
-  </div>
+        <div>
+          @foreach ($categories as $category)
+            <div>
+              <input type="checkbox" name="categories[]" {{ ($album->categories->contains($category)) ? 'checked' : '' }} value="{{ $category->id}}">
+              <span>{{ $category->name }}</span>
+            </div>
+          @endforeach
+        </div>
 
-  <div>
-    @foreach ($categories as $category)
-      <div>
-        <input type="checkbox" name="categories[]" {{ ($album->categories->contains($category)) ? 'checked' : '' }} value="{{ $category->id}}">
-        <span>{{ $category->name }}</span>
-      </div>
-    @endforeach
+        <div>
+          <input class="bottone_salva" type="submit" value="Save">
+        </div>
+      </form>
+    </div>
   </div>
-
-  <div>
-    <input type="submit" value="Save">
-  </div>
-</form>
+@endsection

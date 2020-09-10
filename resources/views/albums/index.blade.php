901f51b25fa44d946a168e1129100612107f6271
lindam <linda.minotti2@gmail.com>
2020-09-10 08:47:59 +0200

    Grafica pagina albums index.

--- resources/views/albums/index.blade.php
+++ resources/views/albums/index.blade.php
@@ -1,9 +1,20 @@
-<h1>Album's list</h1>
-<ul>
-  @foreach ($albums as $album)
-    <li>
-      Title: {{ $album->title }}
-      <a href="{{ route('albums.show', $album)}}">Show</a>
-    </li>
-  @endforeach
-</ul>
+@extends('layouts.layout')
+@section('content')
+  <div class="main_wrapper">
+    <h1>Album's list</h1>
+
+      <div class="music_container">
+        @foreach ($albums as $album)
+          <div class="album_item">
+            <img src="{{ $album->images->url }}" alt=" {{ $album->title }} ">
+            <h2>Title: {{ $album->title }}</h2>
+            <p>Artist: {{ $album->artist }}</p>
+            <p>Year: {{ $album->year }}</p>
+            <a href="{{ route('albums.show', $album)}}">Show</a>
+          </div>
+        @endforeach
+      </div>
+
+
+  </div>
+@endsection
--- resources/views/albums/show.blade.php
+++ resources/views/albums/show.blade.php
@@ -1,14 +1,17 @@
-<h2>Title: {{ $album->title }}</h2>
-<p>Artist: {{ $album->artist }}</p>
-<p>Year: {{ $album->year }}</p>
-<a href="{{ route('albums.index')}}">Back to album's list</a>
-<h3>Song's list</h3>
-<img src="{{ $album->images->url }}" alt=" {{ $album->title }} ">
-<ol>
-  @foreach ($album->songs as $song)
-    <li>
-      {{ $song->title }}
-      Genre: {{ $song->genre }}
-    </li>
-  @endforeach
-</ol>
+@extends('layouts.layout')
+@section('content')
+  <h2>Title: {{ $album->title }}</h2>
+  <p>Artist: {{ $album->artist }}</p>
+  <p>Year: {{ $album->year }}</p>
+  <a href="{{ route('albums.index')}}">Back to album's list</a>
+  <h3>Song's list</h3>
+  <img src="{{ $album->images->url }}" alt=" {{ $album->title }} ">
+  <ol>
+    @foreach ($album->songs as $song)
+      <li>
+        {{ $song->title }}
+        Genre: {{ $song->genre }}
+      </li>
+    @endforeach
+  </ol>
+@endsection

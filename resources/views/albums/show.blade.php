5aa2681e63964cb44a9db85dcc0d2ce296549e5c
lindam <linda.minotti2@gmail.com>
2020-09-10 09:18:18 +0200

    Grafica pagina albums show.

--- resources/views/albums/show.blade.php
+++ resources/views/albums/show.blade.php
@@ -1,17 +1,29 @@
 @extends('layouts.layout')
 @section('content')
-  <h2>Title: {{ $album->title }}</h2>
-  <p>Artist: {{ $album->artist }}</p>
-  <p>Year: {{ $album->year }}</p>
-  <a href="{{ route('albums.index')}}">Back to album's list</a>
-  <h3>Song's list</h3>
-  <img src="{{ $album->images->url }}" alt=" {{ $album->title }} ">
-  <ol>
-    @foreach ($album->songs as $song)
-      <li>
-        {{ $song->title }}
-        Genre: {{ $song->genre }}
-      </li>
-    @endforeach
-  </ol>
+  <div class="main_wrapper">
+    <h1>Title: {{ $album->title }}</h1>
+    <p>Artist: {{ $album->artist }}</p>
+    <p>Year: {{ $album->year }}</p>
+    <div class="music_container">
+      <div class="cover_column">
+        <h2>Cover</h2>
+        <img class="cover_image" src="{{ $album->images->url }}" alt=" {{ $album->title }} ">
+      </div>
+      <div class="songs_list_column">
+        <h2>Song's list</h2>
+
+        <ol>
+          @foreach ($album->songs as $song)
+            <li>
+              {{ $song->title }}
+              Genre: {{ $song->genre }}
+            </li>
+          @endforeach
+        </ol>
+      </div>
+    </div>
+
+    <a href="{{ route('albums.index')}}">Back to album's list</a>
+
+  </div>
 @endsection
